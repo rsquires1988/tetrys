423a48c9b0a98278f07c5ce03a5c372cf308f4eb
rsquires1988 <ryguy88enator@gmail.com>
2023-01-14 20:41:21 -0800

    fast drop and random tetronimo

--- tetris.py
+++ tetris.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import pygame
+import random
+from typing import List
 
 def main():
     side_len = 20
     
     # function to create a single block on its own Surface
     def create_block(block_color):
+        # make block surface the same size as the block itself
         block_surface = pygame.Surface((side_len,side_len))
         
         # give block a background color
@@ -26,7 +29,7 @@ def main():
         
         # make surface to combine all blocks into one asset, and give it a transparent background
         tetronimo_surface = pygame.Surface((40, 60), pygame.SRCALPHA)
-        #* DEBUG: tetronimo_surface.fill((200,200,200,0)) # shows rect of tetronimo surface when last int is non-zero
+        tetronimo_surface.fill((200,200,200,255)) # DEBUG: shows rect of tetronimo surface when last int is non-zero
         
         # make each block in the tetronimo
         for block_position in block_positions:
@@ -102,6 +105,7 @@ def main():
             
         return tetronimo_surface
     
+    # function to create a "T" tetronimo
     def create_t_tetronimo(x=0,y=0):
         block_color = (255, 165, 0) # orange
         block_positions = [(x+side_len,y), (x, y+side_len), (x+side_len, y+side_len), (x+2*side_len, y+side_len)]
@@ -113,6 +117,13 @@ def main():
             tetronimo_surface.blit(block_surface, block_position)
             
         return tetronimo_surface
+    
+    def next_tetronimo(tetronimo_list: List[pygame.Surface]):
+        tetronimo_choice = random.choice(tetronimo_list)
+        
+        return tetronimo_choice
+        # draw tetronimos
+        
     
     # initialize pygame
     pygame.init()
@@ -128,10 +139,50 @@ def main():
     bg_color = (100, 100, 100)
     
     right_l_tetronimo = create_right_l_tetronimo()
-    move_rect = right_l_tetronimo.get_rect()
+    box_tetronimo = create_box_tetronimo()
+    left_l_tetronimo = create_left_l_tetronimo()
+    right_s_tetronimo = create_right_s_tetronimo()
+    straight_tetronimo = create_straight_tetronimo()
+    left_s_tetronimo = create_left_s_tetronimo()
+    t_tetronimo = create_t_tetronimo()
     
+    # tetronimo_list = [create_right_l_tetronimo(), create_box_tetronimo(), create_left_l_tetronimo(),
+    #                   create_right_s_tetronimo(), create_straight_tetronimo(),
+    #                   create_left_s_tetronimo(), create_t_tetronimo()]
+    tetronimo_list = [right_l_tetronimo, box_tetronimo, left_l_tetronimo, right_s_tetronimo,
+                      straight_tetronimo, left_s_tetronimo, t_tetronimo]
+    
+    current_tetronimo = next_tetronimo(tetronimo_list)
+    move_rect = current_tetronimo.get_rect()
+    
+    # tetronimo_list.append(create_right_l_tetronimo())
+    # box_tetronimo = create_box_tetronimo()
+    # create_left
+    # screen.blit(right_l_tetronimo, move_rect)
+    # # screen.blit(create_right_l_tetronimo(), (40, 40))
+    # screen.blit(create_box_tetronimo(), (80, 40))
+    # screen.blit(create_left_s_tetronimo(), (120, 40))
+    # screen.blit(create_right_s_tetronimo(), (40, 120))
+    # screen.blit(create_straight_tetronimo(), (80, 120))
+    # screen.blit(create_left_l_tetronimo(), (120, 120))
+    # screen.blit(create_t_tetronimo(), (80, 200))
+    # move_rect = right_l_tetronimo.get_rect()
+    
+    # initialize game clock
     clock = pygame.time.Clock()
-    pygame.time.set_timer(pygame.USEREVENT,1000)
+    
+    # DEBUG:
+    # frame_count = 0
+    # start_time = time.time()
+    
+    # determines how fast the block moves downward (once per second)
+
+    # speed = 1
+    
+    base_interval = 1000
+    interval = base_interval # milliseconds
+    # remaining_time = interval
+    pygame.time.set_timer(pygame.USEREVENT, interval)
 
     # start the main loop
     done = False
@@ -140,36 +191,58 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            elif event.type == pygame.USEREVENT:
-                    move_rect.y += side_len
+            elif event.type == pygame.USEREVENT:    # timer is at 0
+                # DEBUG:
+                # if interval < 0:
+                #     pygame.time.set_timer(pygame.USEREVENT, interval)
+                move_rect.y += side_len             # move tetronimo down one side_len
+                if move_rect.bottom > screen.get_height():
+                    stay_rect=move_rect
+                    screen.blit(current_tetronimo, stay_rect)
+                    current_tetronimo = next_tetronimo(tetronimo_list)
+                    move_rect = current_tetronimo.get_rect()
             elif event.type == pygame.KEYDOWN:
                 # screen.fill(bg_color)
                 if event.key == pygame.K_LEFT:
                     move_rect.x -= side_len
                 if event.key == pygame.K_RIGHT:
                     move_rect.x += side_len
-                    
-                pygame.display.update()
+                if event.key == pygame.K_DOWN:
+                    interval = interval // 10       # divide length of interval by 10, thereby increasing how fast the tetronimo drops
+                    pygame.time.set_timer(pygame.USEREVENT, interval)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    interval = base_interval        # return "speed" to standard
+                    pygame.time.set_timer(pygame.USEREVENT, interval)
 
         # clear the screen
         screen.fill(bg_color)
         
-
-
+        
         # draw tetronimos
-        screen.blit(right_l_tetronimo, move_rect)
-        # screen.blit(create_right_l_tetronimo(), (40, 40))
-        screen.blit(create_box_tetronimo(), (80, 40))
-        screen.blit(create_left_s_tetronimo(), (120, 40))
-        screen.blit(create_right_s_tetronimo(), (40, 120))
-        screen.blit(create_straight_tetronimo(), (80, 120))
-        screen.blit(create_left_l_tetronimo(), (120, 120))
-        screen.blit(create_t_tetronimo(), (80, 200))
+        screen.blit(current_tetronimo, move_rect)
         
         # update the display
         pygame.display.flip()
         
-        clock.tick(10)
+        # DEBUG:
+        # frame_count += 1
+        # elapsed_time = time.time() - start_time
+        #
+        # if elapsed_time >= 1:
+        #     frame_rate = frame_count / elapsed_time
+        #     print(f"Frame rate: {frame_rate:.2f} fps")
+        #     frame_count = 0
+        #     start_time = time.time()
+        
+
+        # DEBUG: 
+        # if interval != 1000:
+        #     interval = 1000
+        #     pygame.time.set_timer(pygame.USEREVENT, interval)
+        
+        # cap framerate to limit speed that the player can move side to side
+        clock.tick(60)
 
     # close the window and quit pygame
     pygame.quit()
